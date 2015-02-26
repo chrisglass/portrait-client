753b3d5d6b3477125441bc099c51b29648d19333
Chris Glass <tribaal@gmail.com>
2015-02-26 09:43:55 +0300

    Some rudimentary benchmarking code for bpickle.

---
+++ landscape/lib/tests/test_benchmarking.py
@@ -0,0 +1,58 @@
+import unittest
+import time
+import json
+import numpy
+
+from landscape.lib import bpickle
+
+
+@unittest.skip  #XXX: REMOVE BEFORE FLIGHT
+class BpickleBechmarksTest(unittest.TestCase):
+
+    small_dict = {
+        "a string": "something",
+        "a float": 12.45,
+        "an int": 12345,
+        "true!": True,
+        "false": False,
+        "none": None,
+        "dict": {"a": 1, "b": 2, "c": 3},
+        "list": ["a", "b", "c"]}
+
+    def setUp(self):
+        self.big_dict = {}
+
+        for i in range(1000):
+            self.big_dict.update({i: self.small_dict.copy()})
+
+
+    def test_bpickle_vs_json_1000_big_dict(self):
+        """
+        Some crappy benchmarks.
+        """
+        bpickle_results = []
+        json_results = []
+        for i in range(1000):
+
+            # Time the conversion
+            bpickle_start = time.time()
+            result = bpickle.dumps(self.big_dict)
+            assert result is not None
+            bpickle_stop = time.time()
+            json_start = time.time()
+            result2 = json.dumps(self.big_dict)
+            assert result2 is not None
+            json_stop = time.time()
+
+            # Compute elapsed time for each
+            bpickle_elapsed = bpickle_stop - bpickle_start
+            json_elapsed = json_stop - json_start
+
+            # Append for this run's results.
+            bpickle_results.append(bpickle_elapsed)
+            json_results.append(json_elapsed)
+
+        print("Bpickle average: %f" % numpy.mean(bpickle_results))
+        print("Bpickle standard deviation: %f" % numpy.std(bpickle_results))
+        print("JSON average: %f" % numpy.mean(json_results))
+        print("JSON standard deviation: %f" % numpy.std(json_results))
